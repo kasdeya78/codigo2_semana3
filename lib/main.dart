9904794456a98f0bcc99eb02d8cdbad041b2d145
Eduardo Chavez <eduardochavezv@gmail.com>
2022-09-28 17:31:46 -0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 300.00,
-                height: 300.00,
-                color: Colors.amber,
-                child: Image.network(
-                  "https://images.pexels.com/photos/7839624/pexels-photo-7839624.jpeg",
-                  fit: BoxFit.cover,
-                ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 60.0,
+              backgroundColor: Colors.white60,
+              backgroundImage: NetworkImage(
+                  "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg"),
+            ),
+            Text(
+              "Juan Perez Gonzales",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
               ),
-              ElevatedButton(
-                onPressed: () {
-                  print("Hola!!");
-                },
-                child: Text("Presiones Aquí"),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                color: Colors.white60,
+                fontSize: 14.0,
+                letterSpacing: 3.0,
+                fontWeight: FontWeight.w400,
               ),
-              TextButton(
-                onPressed: () {
-                  print("Hola 2!!");
-                },
-                child: Text("Presiones Aquí"),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
